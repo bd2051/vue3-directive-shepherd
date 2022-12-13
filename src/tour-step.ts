fc173e0347b542207001bfb18158062d19766eb7
bd2051 <bd2051@mail.ru>
2022-12-14 00:27:07 +0300

    fix

--- src/tour-step.ts
+++ src/tour-step.ts
@@ -2,15 +2,17 @@ const tourStep = {
   created(el, binding) {
     const step = binding.arg;
     const { tour, options } = binding.value;
-    if (!options.attachTo) {
-      options.attachTo = {};
+    if (!tour.getById(step)) {
+      if (!options.attachTo) {
+        options.attachTo = {};
+      }
+      options.attachTo.element = el;
+      tour.addStep({
+        id: step,
+        ...options,
+      });
+      tour.steps.sort((a, b) => a.id - b.id);
     }
-    options.attachTo.element = el;
-    tour.addStep({
-      id: step,
-      ...options,
-    });
-    tour.steps.sort((a, b) => a.id - b.id);
   },
 };
 
